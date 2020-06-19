dd440211bd9a25bdc820d518f1ad515c289e96b9
Elinvynia <59487684+Elinvynia@users.noreply.github.com>
2020-06-19 21:46:59 +0200

    Remove Discord link

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,8 @@
-//! [![ci-badge][]][ci] [![docs-badge][]][docs] [![crate-version]][crate-link] [![guild-badge][]][guild]
+//! [![ci-badge][]][ci] [![docs-badge][]][docs] [![crate-version]][crate-link]
 //!
 //! Tranquility is an asynchronous Rust library for the Reddit API.
 //!
-//! It is currently under heavy development, and as such it is not recommended for production or any other use. If you'd like to help with development, you are welcome to join our Discord community.
+//! It is currently under heavy development, and as such it is not recommended for production or any other use.
 //!
 //!
 //! [ci]: https://github.com/Elinvynia/tranquility/actions?query=workflow%3ARust
@@ -11,8 +11,6 @@
 //! [docs-badge]: https://img.shields.io/badge/docs-online-5023dd.svg?style=flat-square
 //! [crate-link]: https://crates.io/crates/tranquility
 //! [crate-version]: https://img.shields.io/crates/v/tranquility.svg?style=flat-square
-//! [guild]: https://discord.gg/WKtbGHp
-//! [guild-badge]: https://img.shields.io/discord/694655186232279170.svg?style=flat-square&colorB=7289DA
 
 #![warn(missing_docs)]
 #![warn(missing_debug_implementations)]
